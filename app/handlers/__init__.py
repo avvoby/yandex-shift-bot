64087b983018404fd7760d6b589c213f59905d8d
avvoby <avvoby@gmail.com>
2026-03-11 15:57:26 +0300

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,1 +1,23 @@
+from aiogram import Dispatcher
 
+from app.handlers.registration import router as registration_router
+from app.handlers.user_menu import router as user_menu_router
+from app.handlers.faq import router as faq_router
+from app.handlers.ask_question import router as ask_question_router
+from app.handlers.admin import router as admin_router
+from app.handlers.shift_poll import router as shift_poll_router
+from app.handlers.common import router as common_router
+
+
+def register_all_routers(dp: Dispatcher) -> None:
+    """
+    Подключаем роутеры к диспетчеру.
+    Сначала специальные сценарии, потом общие.
+    """
+    dp.include_router(registration_router)
+    dp.include_router(user_menu_router)
+    dp.include_router(faq_router)
+    dp.include_router(ask_question_router)
+    dp.include_router(admin_router)
+    dp.include_router(shift_poll_router)
+    dp.include_router(common_router)
